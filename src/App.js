88064c7e6d45dc306cf6c30b25f0ae5b746ebe4a
deeksha16222 <56512924+deeksha16222@users.noreply.github.com>
2021-01-16 18:09:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, {useState} from "react";
-const App = () => {
-const purple = "#8e44ad";
-const [bg, setBg] = useState(purple);
-const [name, setName] = useState("click me");
-
-const bgChange = () => {
-let newBg = "#34495e";
-setBg(newBg);
-setName("hello there");
+import React, { useState } from "react";
+const App = (props) => {
+  return(
+<>
+<div className="main_div">
+<div className="center_div">
+<br/>
+<h1> ToDoList </h1>
+<br/>
+<input type="text" placeholder="Add an item"/>
+<button> + </button>
+<ol>
+<li> buy apple </li>
+</ol>
+</div>
+</div>
+</>
+);
 };
- 
-const bgBack = () => {
-setBg(purple);
-setName("click me");
-}
-
- 
-   return (
-     <>
-     <div style = {{ backgroundColor: "bg" }}>
-     <button onClick={bgChange} onDoubleClick={bgBack}> {name} </button>
-     </div>
-     </>
-   );
- }
-
 export default App;
